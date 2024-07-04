1aa67e984fdbd2488faee9a9898f0a082a48c676
Taylor Hunt <62562348+Airamentis@users.noreply.github.com>
2024-07-04 12:50:55 -0700

    updated rolling enemy

--- scripts/rolling_enemy.gd
+++ scripts/rolling_enemy.gd
@@ -1,7 +1,14 @@
 extends Node2D
+
 @onready var ray_cast_left = $RayCastLeft
-var rotationSpeed = 50
+@onready var timer = $Timer
+
+var rotationSpeed = -2
 var shouldRotate = false
+var direction = -1
+var attackSpeed = 30
+var rotationMax = 25
+var attackSpeedMax = 375
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +20,15 @@ func _ready():
 func _process(delta):
 	if shouldRotate:
 		rotation += rotationSpeed * delta
+		position.x += direction * attackSpeed * delta
 	if ray_cast_left.is_colliding():
+		timer.start()
 		shouldRotate = true
+
+
+
+func _on_timer_timeout():
+	if rotationSpeed <= rotationMax:
+		rotationSpeed = rotationSpeed * 1.5
+	if attackSpeed <= attackSpeedMax:
+		attackSpeed = attackSpeed * 1.5
