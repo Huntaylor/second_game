31e4c4fa6c521c4b3cbc83ac41d4ce052e4dcb66
Taylor Hunt <62562348+Airamentis@users.noreply.github.com>
2024-07-08 16:17:37 -0700

    added more folders for organization

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,13 +1,23 @@
 extends Node
 
+@onready var timer = $Timer
+
 signal flash_animation
 signal player_death
 
 var lives = 3
+var isHit = false
 
 func take_damage():
-	emit_signal('flash_animation')
-	lives = lives - 1
-	if lives <= 0:
-		emit_signal('player_death')
+	if !isHit:
+		isHit = true
+		timer.start()
+		emit_signal('flash_animation')
+		lives = lives - 1
+		if lives <= 0:
+			emit_signal('player_death')
 
+
+
+func _on_timer_timeout():
+	isHit = false
